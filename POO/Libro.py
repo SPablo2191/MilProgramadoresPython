5186230fe3f94ab69174f857d9e6e75b07464444
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-10-12 18:58:28 -0300

    fix: libro.py setter

--- POO/Libro.py
+++ POO/Libro.py
@@ -1,34 +1,42 @@
 class Libro:
-    def __init__(self,titulo,autor,cantEjemplares,cantEjemplaresPrestados=0):
+    def __init__(self, titulo, autor, cantEjemplares, cantEjemplaresPrestados=0):
         self.__titulo = titulo
         self.__autor = autor
         self.__cantEjemplares = cantEjemplares
         self.__cantEjemplaresPrestados = cantEjemplaresPrestados
+
     @property
     def cantEjemplaresPrestados(self):
         return self.__cantEjemplaresPrestados
+
     @cantEjemplaresPrestados.setter
-    def cantEjemplaresPrestados(self,valor):
+    def cantEjemplaresPrestados(self, valor):
         self.__cantEjemplaresPrestados = self.__cantEjemplaresPrestados + valor
+
     def prestar(self):
-        if(self.__cantEjemplares!= self.__cantEjemplaresPrestados):
-            self.cantEjemplaresPrestados(1)
+        if (self.__cantEjemplares != self.__cantEjemplaresPrestados):
+            self.cantEjemplaresPrestados = 1 # python trabaja directamente con la variable
             print("El prestamo se realizo con exito!")
         else:
             print("no se puede realizar el prestamo")
+
     def devolver(self):
-        if(self.__cantEjemplaresPrestados>0):
-            self.__cantEjemplaresPrestados-=1
+        if (self.__cantEjemplaresPrestados > 0):
+            self.cantEjemplaresPrestados = -1
             print("La devolucion se realizo con exito!")
         else:
             print("Devolucion denegada")
+
     def __str__(self):
         return f"{self.__titulo} / {self.__autor} / {self.__cantEjemplares} / {self.__cantEjemplaresPrestados}"
-librin = Libro("sandokan","gonzalo Oropeza",3)
+
+
+librin = Libro("sandokan", "gonzalo Oropeza", 3)
 print(librin)
 librin.prestar()
 print(librin)
 librin.prestar()
+print(librin.cantEjemplaresPrestados)
 librin.prestar()
 librin.prestar()
 print(librin)
